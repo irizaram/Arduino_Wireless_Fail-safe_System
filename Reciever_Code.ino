1025ac77f9f00b53e95ef18ffd50e32a70ff11b1
irizaram <irizaram@miamioh.edu>
2016-03-15 00:32:50 -0400

    Added files via upload

---
+++ Reciever_Code.ino
@@ -0,0 +1,67 @@
+//Reciever Code
+
+//Libraries
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//Initialize Variables and Transciever Code
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int indicatorLED = 3;
+
+//Setup Void Code
+void setup(void){
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+  pinMode(indicatorLED, OUTPUT);
+}
+
+//Void Loop Code
+void loop(void){
+
+//Declare Boolean Variable
+//  currentStatus - used to swap statuses of the reciever 
+bool currentStatus = true;
+
+//Checks for radio connection
+ if (radio.available()){
+   bool terminate = false;  
+   
+   //Always checks if termination command given (button)
+   while (!terminate){
+     terminate = radio.read(msg, 1);      
+     
+     //If the termination switch has been detected
+     if (msg[0] == 111 ){
+       currentStatus = true;
+       delay(10);
+       Serial.println("WARNING! TERMINATION PROTOCALS ACTIVATED! COMMENCING EMERGENCY SHUTDOWN!");
+       digitalWrite(indicatorLED, LOW);
+       terminate = true;
+       delay(10);
+     }
+     
+     //If the termination switch hasn't been detected yet
+     else{
+       Serial.println("All systems are normal");
+       digitalWrite(indicatorLED, HIGH);
+   }
+   }
+ }
+ //If there is no detection of the transmitter, the reciever will shut down the motor
+ else{
+   //Check to see if transmitter is being detected
+   if(msg[0] == 500){
+     Serial.println("All systems are normal");
+     digitalWrite(indicatorLED, HIGH);
+   }
+   else{
+   Serial.println("WARNING! LOST CONNECTION WITH TRANSMITTER! COMMENCING EMERGENCY SHUTDOWN!");
+   digitalWrite(indicatorLED, LOW);
+   }
+ }
+}
---
+++ Transmitter_Code.ino
@@ -0,0 +1,38 @@
+  //Transmitter Code
+  
+  //Libraries
+  #include  <SPI.h>
+  #include "nRF24L01.h"
+  #include "RF24.h"
+  
+  //Initialize variables and Transciever Code
+  int msg[1];
+  RF24 radio(9,10);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  int inputSwitch = 7;
+  
+  //Setup Void Code
+  void setup(void){
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(pipe);
+  }
+  
+  //Loop Void Code
+  void loop(void){
+    
+   //If the termination switch is pushed down
+   if (digitalRead(inputSwitch) == HIGH){
+     
+     //Transmit to the other transmitter the "terminate code"
+     msg[0] = 111;
+     radio.write(msg, 1);
+   }
+   
+   //If the terminatoin switch is not being pushed down
+   else{
+     //Transmit the "all clear" code
+     msg[0]=500;
+     radio.write(msg, 1);
+     }
+   }
