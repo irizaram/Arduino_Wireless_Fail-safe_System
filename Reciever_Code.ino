bb4bbd4f197c9b6c3e84755a4889f221033fb4a1
irizaram <irizaram@miamioh.edu>
2016-03-15 23:17:37 -0400

    Added files via upload
    These are the transmitter and receiver codes I used to create this project

---
+++ Reciever_Code.ino
@@ -0,0 +1,58 @@
+//Reciever Code
+
+//Libraries
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//Initialize Variables and Transciever Code
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int indicatorLED = 6;
+
+//Setup Void Code
+void setup(void){
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+  pinMode(indicatorLED, OUTPUT);
+}
+
+//Void Loop Code
+void loop(void){
+
+//Declare Boolean Variable
+
+//Checks for radio connection
+ if (radio.available()){
+   bool terminate = false;  
+   
+   //Always check for a command to be given from the transmitter
+   while (!terminate){
+     //Read the transmission being given from the transmiter
+     terminate = radio.read(msg, 1);      
+     delay(50);
+     //If the termination button has been pressed
+     if (msg[0] == 50 ){
+       Serial.println("WARNING! TERMINATION PROTOCOLS ACTIVATED! COMMENCING EMERGENCY SHUTDOWN!");
+       digitalWrite(indicatorLED, LOW);
+       terminate = true;
+     }
+     //If the command to either deactivate the emergency protocals or to resume progress has been given
+     else if(msg[0] == 90){
+       Serial.println("All systems normal");
+       digitalWrite(indicatorLED, HIGH);
+     }
+     delay(50);
+     }
+   }
+ //If there is no detection of the transmitter, the reciever will shut down the motor
+ else{
+   Serial.println("WARNING! LOST CONNECTION WITH TRANSMITTER! COMMENCING EMERGENCY SHUTDOWN!");
+   digitalWrite(indicatorLED, LOW);
+   delay(100);
+ }
+}
+
---
+++ Transmitter_Code.ino
@@ -0,0 +1,49 @@
+  //Transmitter Code
+  
+  //Libraries
+  #include  <SPI.h>
+  #include "nRF24L01.h"
+  #include "RF24.h"
+  
+  //Initialize variables and Transciever Code
+  int msg[1];
+  RF24 radio(9,10);
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
+  int inputSwitch = 8;
+  boolean currentStatus = false;
+  
+  //Setup Void Code
+  void setup(void){
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(pipe);
+  }
+  
+  //Loop Void Code
+  void loop(void){
+    
+   //If the termination switch is pushed down
+   if (digitalRead(inputSwitch) == HIGH){
+     
+     //If the current status is all clear, transmit to the "emergency protocol command"
+     if(currentStatus == false){
+       currentStatus = true;
+     }
+     //If the "emergency protocol command" is being given, and the button is pressed again, cancel the command, and resume proper protocol
+     else{
+       currentStatus = false;
+     }
+     delay(100);
+   }
+   //Determinne the kind of signal to transmit
+   //-If the "emergency protocol command" has been given
+   if(currentStatus == true){
+     msg[0] = 50;
+   }
+   //-If the "emergency protocol command" has not been given, or has been canceled
+   else if(currentStatus == false){
+     msg[0]=90;
+   }
+   //Transmit the coommand to the receiver
+   radio.write(msg, 1);
+ }
